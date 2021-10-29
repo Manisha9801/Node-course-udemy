1a4298b7dca3dea139ef68bf4cc82d2b8d0428dc
Manisha Kumari <manisha.9801@gmail.com>
2021-10-29 11:12:58 +0530

    assignment-3 changes

--- Express/app.js
+++ Express/app.js
@@ -1,16 +1,26 @@
-const http = require('http')
+ //core module
+const path = require('path') 
 
-const express = require('express')
+//3rd party module
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
+//local module
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-app.use('/add-product',(req,res,next) => {
-    console.log('In msg middleware')
-    res.send('<h1>In Add-Product Page</h1>')
+const app = express();
+
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
+
+app.use(express.static(path.join(__dirname,'public')))  
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req,res,next) => {  //since we are not specifying path it will take '/' by default
+    // res.status(404).send('<h1>Page Not Found!!</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','page-not-found.html'))
 })
-app.use('/',(req,res,next) => {
-    console.log('In the middleware which always runs')
-    res.send('<h1>This always runs!</h1>')
-})
 
-app.listen(3000)  //this function internally does the job of creating server and listening it. See docs to get more idea from express github
+app.listen(3000); //this function internally does the job of creating server and listening it. See docs to get more idea from express github
